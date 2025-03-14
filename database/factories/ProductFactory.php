160e214ecf6814233e46d159273a62ac7d358332
alihamdan2020 <ali_hamdane@hotmail.com>
2025-03-15 01:00:26 +0200

    first init

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+           'product_name'=>$this->faker->name,
+           'category_id'=>$this->faker->numberBetween(1,10),
+           'product_price'=>$this->faker->randomFloat(1,10,100),
+           'product_description'=>$this->faker->text,
+           'product_photo'=>'https://placehold.co/600x400?text=Hello+World'
+        ];
+    }
+}
